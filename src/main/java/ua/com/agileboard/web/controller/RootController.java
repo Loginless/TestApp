2c922d5a98e2c557eda4853ee1bb8bc5022b902f
Loginless <Loginless@bigmir.net>
2019-03-26 00:07:16 +0200

    Agile board 1.12 Add authorization via separate login page Add ajax to the Users table

--- src/main/java/ua/com/agileboard/web/controller/RootController.java
+++ src/main/java/ua/com/agileboard/web/controller/RootController.java
@@ -1,37 +1,25 @@
 package ua.com.agileboard.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import ua.com.agileboard.service.UserService;
-import ua.com.agileboard.util.SecurityUtil;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RootController {
 
 
-    @Autowired
-    private UserService userService;
-
     @GetMapping("/")
     public String root() {
-        return "index";
+        return "redirect:users";
     }
 
     @GetMapping("/users")
-    public String users(Model model) {
-        model.addAttribute("users", userService.getAll());
+    public String users() {
         return "users";
     }
 
-    @PostMapping("/users")
-    public String setUser(HttpServletRequest request) {
-        int userId = Integer.valueOf(request.getParameter("userId"));
-        SecurityUtil.setAuthUserId(userId);
-        return "redirect:users";
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
     }
+
 }
--- src/main/webapp/fragments/headTag.jsp
+++ src/main/webapp/fragments/headTag.jsp
@@ -14,9 +14,10 @@
     <link rel="stylesheet" href="webjars/noty/3.1.4/lib/noty.css"/>
 
     <%--http://stackoverflow.com/a/24070373/548473--%>
-    <script type="text/javascript" src="webjars/jquery/3.3.1-1/jquery.min.js" defer></script>
+    <script type="text/javascript" src="webjars/jquery/3.3.1-1/jquery.min.js"></script>
     <script type="text/javascript" src="webjars/bootstrap/4.1.3/js/bootstrap.min.js" defer></script>
     <script type="text/javascript" src="webjars/datatables/1.10.19/js/jquery.dataTables.min.js" defer></script>
     <script type="text/javascript" src="webjars/datatables/1.10.19/js/dataTables.bootstrap4.min.js" defer></script>
     <script type="text/javascript" src="webjars/noty/3.1.4/lib/noty.min.js" defer></script>
+    <script type="text/javascript" src="webjars/datetimepicker/2.5.20/build/jquery.datetimepicker.full.min.js" defer></script>
 </head>
